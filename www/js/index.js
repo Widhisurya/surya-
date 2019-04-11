9797c7f140be4f7d7c9613459eafc2b11823cc55
Widhisurya <48555001+Widhisurya@users.noreply.github.com>
2019-04-11 19:17:07 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=oStWyLH9aKcLTBlUgpm5nXpI1h2AxhEBGQsNqNU0";
         
         $("#loadImage").click(function(){
             $.ajax({
